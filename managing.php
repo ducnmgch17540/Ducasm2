d1c1b957b8a5ff519da9866340a20f705db2d050
hoangchanlong <longnhgch18327@fpt.edu.vn>
2019-11-07 00:49:15 +0700

    css

--- managing.php
+++ managing.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/style.css">
-    <link rel="stylesheet" href="test.css">
+    <link rel="stylesheet" href="/test.css">
     <title>Document</title>
     <script src="https://kit.fontawesome.com/3fa7701fe0.js" crossorigin="anonymous"></script>
 
@@ -43,7 +43,7 @@
             <?php
             }
             ?> 
-            
+
             </thead>
             
         </table>
