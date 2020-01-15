82a61241f36b46e3aa941ad72382ae999805557d
Nguyen Minh Duc <kenalexander103@gmail.com>
2020-01-16 00:53:16 +0700

    Ducasm2

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php	
     $db = parse_url(getenv("DATABASE_URL"));
     $pdo = new PDO("pgsql:" . sprintf(
-        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        "host=ec2-174-129-29-52.compute-1.amazonaws.com;port=%s;user=uezqbwprhjylep;password=710fdd6a7800595e8a5655c1b10556123db6ae89b9ec20c4f56b1d2042fed955;dbname=d6769srljenbet",
         $db["host"],
         $db["port"],
         $db["user"],
