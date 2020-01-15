989e2d3700b03353526f504d86f7e73aef34d971
Nguyen Minh Duc <kenalexander103@gmail.com>
2020-01-16 01:14:56 +0700

    Ducasm2

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$conn = pg_connect("host=ec2-107-21-104-31.compute-1.amazonaws.com port=5432 dbname=d1rfv0n3kq9ql user=cyicicbmijkhvq password=d19129b46ee5527abd853339b1253196b8872f3bb44093443273e1ac12bf9c03") 
+$conn = pg_connect("host=ec2-174-129-29-52.compute-1.amazonaws.com port=5432 dbname=d6769srljenbet user=uezqbwprhjylep password=710fdd6a7800595e8a5655c1b10556123db6ae89b9ec20c4f56b1d2042fed955") 
 or die("Connect Unsuccessful");
 ?>
